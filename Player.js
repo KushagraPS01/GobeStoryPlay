5c4a1f5673b8d5b848b726fb71bd5cec63bb6fe0
KushagraPS01 <kushagrapratap01@gmail.com>
2024-10-06 12:29:09 +0530

    Gobe Play Insert

---
+++ Player.js
@@ -0,0 +1,9 @@
+const videoElement = document.getElementById('myVideo');
+
+        function playVideo() {
+            videoElement.play();
+        }
+
+        function pauseVideo() {
+            videoElement.pause();
+        }
